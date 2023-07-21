5d63be13d33b5d5bd5de66434d466c2731af4ca0
Deepanshu Kohli <deepanshukohli47@gmail.com>
2023-07-21 14:53:36 +0530

    header Modal Modified

--- src/Components/HeaderModal.js
+++ src/Components/HeaderModal.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react'
 import "../Style/HeaderModal.css"
 const HeaderModal = (props) => {
-  const [headers,setHeaders] = useState([])
+  const [header,setHeader] = useState("")
+  const [allHeaders,setAllheaders] = useState([])
+  const handleHeader = (event)=>{
+    event.preventDefault()
+    if (header.length)
+    {
+    let newHeader = {header:header}
+    setAllheaders([...allHeaders,newHeader])
+    setHeader("")
+    console.log(header)
+    console.log(allHeaders)
+    alert("Header Added Successfully")
+  }
+  else{
+    alert("Please Enter Header")
+  }
+
+  }
   return (
     <div className='head-modal'>
       <div className='head-form'>
            <button className='head-close' onClick={()=>{props.setHeadModal(false)}}>X</button>
-            <form type="action">
+            <form type="action" onSubmit={handleHeader}>
             <label >Header Name</label>
-            <input placeholder="Enter Name"type = "text" className='head-inp' />
-            <button className='submit-head-btn' type="submit">Submit </button>
+            <input placeholder="Enter Name"type = "text" className='head-inp' value={header} onChange={(event) =>{setHeader(event.target.value)}} />
+            <button className='submit-head-btn'  type="submit">Submit </button>
             </form>
       </div>
     </div>
